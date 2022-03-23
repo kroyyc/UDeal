1af58533ddfd3b40673883f1f57bbd4c20d204f5
Bennett Gunson <bengunson@gmail.com>
2022-03-23 16:43:42 -0600

    add starting API controllers

---
+++ UDeal/Controllers/SchoolsController.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using UDeal.Data;
+using UDeal.Models;
+
+namespace UDeal.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SchoolsController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public SchoolsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Schools
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<School>>> GetSchools()
+        {
+            return await _context.Schools.ToListAsync();
+        }
+
+        // GET: api/Schools/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<School>> GetSchool(int id)
+        {
+            var school = await _context.Schools.FindAsync(id);
+
+            if (school == null)
+            {
+                return NotFound();
+            }
+
+            return school;
+        }
+
+        // PUT: api/Schools/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutSchool(int id, School school)
+        {
+            if (id != school.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(school).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!SchoolExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Schools
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<School>> PostSchool(School school)
+        {
+            _context.Schools.Add(school);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetSchool", new { id = school.Id }, school);
+        }
+
+        // DELETE: api/Schools/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteSchool(int id)
+        {
+            var school = await _context.Schools.FindAsync(id);
+            if (school == null)
+            {
+                return NotFound();
+            }
+
+            _context.Schools.Remove(school);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool SchoolExists(int id)
+        {
+            return _context.Schools.Any(e => e.Id == id);
+        }
+    }
+}
